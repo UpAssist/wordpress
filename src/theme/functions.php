c9c5d444754055bb28fb056be5b74b6efa9f9713
Henjo Hoeksma <henjo@upassist.com>
2023-12-22 16:17:01 +0100

    fix: overriding defaults not working

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,19 +1,21 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$theme = [];
-$theme['name'] = 'upassist';
-$theme['version'] = '1.0.0';
-$theme['stylesheets'] = [
-    [
-        'handle' => 'main',
-        'file' => 'assets/styles/main.css',
-        'depends_on' => null,
-        'version' => $theme['version'],
-        'media' => 'all'
-    ]
-];
-$theme['headerScripts'] = [];
-$theme['bodyScripts'] = [];
+// Set this in your implementation
+//$theme = [];
+//$theme['name'] = 'upassist';
+//$theme['version'] = '1.0.0';
+//$theme['stylesheets'] = [
+//    [
+//        'handle' => 'main',
+//        'file' => 'assets/styles/main.css',
+//        'depends_on' => null,
+//        'version' => $theme['version'],
+//        'media' => 'all'
+//    ]
+//];
+//$theme['headerScripts'] = [];
+//$theme['bodyScripts'] = [];
 
-$upassist = new \UpAssist\WordPress\Theme($theme['name'], 'nl_NL', null, $theme['stylesheets']);
+// Replace $upassist with your theme name
+//$upassist = new \UpAssist\WordPress\Theme($theme['name'], 'nl_NL', null, $theme['stylesheets']);
